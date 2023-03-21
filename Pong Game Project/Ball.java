f868b97471de045d183b2c99ac330cb550189b07
Yash Dashpute <71089025+YashGithub31@users.noreply.github.com>
2023-03-21 21:34:44 +0530

    Update Ball.java

--- Pong Game Project/Ball.java
+++ Pong Game Project/Ball.java
@@ -1,25 +1,38 @@
 import java.awt.*;
+import java.util.Random;
 
 public class Ball extends Rectangle {
     int xVelocity;
     int yVelocity;
+    Ball(int x,int y, int width, int height)
+    {
+        super(x,y,width,height); // this is for rectangle class
 
-    Ball(int x, int y, int width, int height) {
-        super(x, y, width, height);
-        xVelocity = 5;
+        Random random = new Random();
+        int r = random.nextInt(2);
+        if(r==1)
+        {
+            xVelocity = -5;
+        }
+        else
+        {
+            xVelocity = 5;
+        }
+
         yVelocity = 5;
 
     }
 
-    public void move() {
-        x += xVelocity;
-        y += yVelocity;
+    public void move()
+    {
+        x = x+xVelocity;
+        y = y+yVelocity;
     }
 
-    public void draw(Graphics g) {
-        g.setColor(Color.white);
-
-        g.fillOval(x, y, width, height);
+    public void draw(Graphics g)
+    {
+        g.setColor(Color.WHITE);
+        g.fillOval(x,y,width,height);
+        g.drawLine(1000/2,0,1000/2,555);
     }
-
 }
