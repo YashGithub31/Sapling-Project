6fef4fd4cca7f326a9b5360f9534900b40b80737
Yash Dashpute <71089025+YashGithub31@users.noreply.github.com>
2023-03-21 21:36:19 +0530

    Update GameFrame.java

--- Pong Game Project/GameFrame.java
+++ Pong Game Project/GameFrame.java
@@ -4,12 +4,20 @@ import java.awt.*;
 public class GameFrame {
     JFrame frame;
     Table table;
-
-    GameFrame() {
+    GameFrame()
+    {
         frame = new JFrame("Pong Game");
+
+
+        // Creating Panel or Table
         table = new Table();
         table.setBackground(Color.black);
+
+        // Adding the table to my frame
         frame.add(table);
+
+        // for frame
+
         frame.setResizable(false);
         frame.setBackground(Color.black);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
