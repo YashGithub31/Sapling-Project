a53f522ee1698faace1815b299c675586788cdec
Yash Dashpute <71089025+YashGithub31@users.noreply.github.com>
2023-03-21 21:37:42 +0530

    Update Paddle.java

--- Pong Game Project/Paddle.java
+++ Pong Game Project/Paddle.java
@@ -2,47 +2,80 @@ import java.awt.*;
 import java.awt.event.KeyEvent;
 
 public class Paddle extends Rectangle {
-
     int id;
-    int yVelocity = 2;
 
-    Paddle(int x, int y, int PADDLE_WIDTH, int PADDLE_HEIGHT, int id) {
-        super(x, y, PADDLE_WIDTH, PADDLE_HEIGHT);
-        this.id = id;
+    int yVelocity;
+    Paddle(int x,int y, int PADDLE_WIDTH,int PADDLE_HEIGHT,int id)
+    {
+        super(x,y,PADDLE_WIDTH,PADDLE_HEIGHT);
+        this.id= id;
     }
 
-    public void draw(Graphics g) {
-        if (id == 1)
+    public void draw(Graphics g)
+    {
+        if(id==1)
+        {
             g.setColor(Color.red);
+        }
 
-        if (id == 2)
+        if(id==2)
+        {
             g.setColor(Color.blue);
-
-        g.fillRect(x, y, width, height);
+        }
+        g.fillRect(x,y,width,height);
     }
 
-    public void move() {
-        y += yVelocity;
+    public void move()
+    {
+        y = y+yVelocity;
     }
 
-    public void keyReleased(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-            yVelocity = 0;
+    public void keyReleased(KeyEvent e)
+    {
+        switch (id) {
+            case 1 -> {
+                if (e.getKeyCode() == KeyEvent.VK_UP) {
+                    yVelocity = 0;
+                }
+                if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+                    yVelocity = 0;
+                }
+            }
+            case 2 -> {
+                if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+                    yVelocity = 0;
+                }
+                if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+                    yVelocity = 0;
+                }
+            }
         }
 
-        if (e.getKeyCode() == KeyEvent.VK_UP) {
-            yVelocity = -2;
+
+
+    }
+
+    public void keyPressed(KeyEvent e)
+    {
+        switch (id) {
+            case 1 -> {
+                if (e.getKeyCode() == KeyEvent.VK_UP) {
+                    yVelocity = -5;
+                }
+                if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+                    yVelocity = 5;
+                }
+            }
+            case 2 -> {
+                if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+                    yVelocity = 5;
+                }
+                if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+                    yVelocity = -5;
+                }
+            }
         }
     }
 
-    public void keyPressed(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_UP) {
-            yVelocity = -2;
-        }
-
-        if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-            yVelocity = 2;
-        }
-    }
 
 }
